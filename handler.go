d859913488c48a9713de55eb4cd81c9b8db4f132
M Ilham Hidayat <ilham@cocowork.co>
2018-09-22 17:43:35 +0700

    Finish create url yaml shortener

--- handler.go
+++ handler.go
@@ -2,6 +2,8 @@ package urlshort
 
 import (
 	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -37,7 +39,37 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+func YAMLHandler(yamlBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	// TODO: Implement this...
-	return nil, nil
+	// 1. Parse the yaml
+	pathUrls, err := parseYAML(yamlBytes)
+	if err != nil {
+		return nil, err
+	}
+	// 2. Convert YAML array into map
+	pathsToUrls := buildMap(pathUrls)
+	// 3. return a map handler using the map
+	return MapHandler(pathsToUrls, fallback), nil
+}
+
+func buildMap(pathUrls []PathUrl) map[string]string {
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+	return pathsToUrls
+}
+
+func parseYAML(data []byte) ([]PathUrl, error) {
+	var pathUrls []PathUrl
+	err := yaml.Unmarshal(data, &pathUrls)
+	if err != nil {
+		return nil, err
+	}
+	return pathUrls, nil
+}
+
+type PathUrl struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
